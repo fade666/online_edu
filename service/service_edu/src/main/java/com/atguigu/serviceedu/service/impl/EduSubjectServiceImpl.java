85eb0f29468b0a4d97140f3216ef834c4ab2c04a
mawei <1136735603@qq.com>
2021-06-20 23:46:51 +0800

    修改nacos地址，mysql地址，初始化项目

--- service/service_edu/src/main/java/com/atguigu/serviceedu/service/impl/EduSubjectServiceImpl.java
+++ service/service_edu/src/main/java/com/atguigu/serviceedu/service/impl/EduSubjectServiceImpl.java
@@ -12,6 +12,7 @@ import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import org.springframework.beans.BeanUtils;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
